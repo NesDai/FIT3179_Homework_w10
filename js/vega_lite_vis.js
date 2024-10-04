efad30893c50fc222290ac7cd9f29c1103c68261
Cheah Tim Leh <tche0082@student.monash.edu>
2024-10-05 00:37:19 +0800

    fds

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,5 +1,12 @@
 var spec2 = "js/map.json";
-vegaEmbed("#bar_chart", spec2)
+vegaEmbed("#chart_1", spec2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var spec3 = "js/bar_chart.json";
+vegaEmbed("#chart_2", spec3)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
