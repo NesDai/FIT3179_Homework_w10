e9bde80b2526075d8e5edcbdfcd0f94825f24d0c
NesDai <tche0082@student.monash.edu>
2024-10-05 19:41:45 +0800

    fx some small stuff

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -20,38 +20,8 @@ vegaEmbed("#chart_1", spec2)
 //   .catch(console.error);
 
 var spec5 = "js/combined_chart.json";
-// vegaEmbed("#chart_4", spec5)
-//   .then(function (result) {
-//     // Access the Vega view instance
-//     const view = result.view;
-
-//     // Set up the button click event listener
-//     document
-//       .getElementById("printSelectionButton")
-//       .addEventListener("click", function () {
-//         // Get the current selection from the view's data
-//         const selectedStates = view.signal("stateSelection"); // Change here
-
-//         // Print the selected states to the console
-//         console.log(selectedStates);
-//       });
-//   })
-//   .catch(console.error);
-
 vegaEmbed("#chart_4", spec5)
   .then(function (result) {
-    const view = result.view;
-
-    // Event listener for the dropdown
-    document
-      .getElementById("stateSelector")
-      .addEventListener("click", function () {
-        const selectedState = this.value;
-        console.log(view.signal("stateSelection"))
-
-        // Update the selection in Vega-Lite
-        view.signal("stateSelection", { state: [selectedState] });
-        view.run();
-      });
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
